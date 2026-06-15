bab0b007248798320b3cd5ebc86b145929252300
mambo06 <mambo06@users.noreply.github.com>
2026-06-15 23:12:21 +1000

    macOs client
    add client

---
+++ Client/README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5c3cfaa44a412c3b391e76ce035cfbc279eea9100fb4c2d5d0008d67249a3eaf
+size 3461
---
+++ Client/macOs.app/Contents/Resources/python_bundle/macos/install_dependencies.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1d0e2550194504fd785382639a1807799f6d4c8e7d0929f9bf2be6881d52b44d
+size 1077
